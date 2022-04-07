c9ddc9c284fd928b6a773fe113d597a80bf18477
VictorEnache <victor22enache@gmail.com>
2022-04-07 17:45:59 +0200

    aggiunti copmmenti al codice

--- main.js
+++ main.js
@@ -31,33 +31,47 @@ const team = [
     },
   ];
 
-
+//creo una variabile per il contenitore delle card
 const container = document.querySelector('.container')
-
+//creo una variabile per il bottone aggiungi
 const button_add = document.getElementById('button')
+//creo una variabile per l'imput name
 const name_1 = document.getElementById('name')
+//creo una variabile per l'imput role
 const role = document.getElementById('role')
+//creo una variabile per l'imput del url per l'immagine
 const url = document.getElementById('url')
 
+//al bottone aggiunngo un Event listener al click
 button_add.addEventListener('click', () =>{
+    //Se tutti e tre gli imput hanno un contenuto testuale allora puo essere eseguito il codice sotto
     if(name_1.value !== '' && role.value !== '' && url.value !== ''){
+        //creio un oggetto che ha come proprietà i valori inseriti nell'imput
         const new_member = {name: name_1.value, role: role.value, image: url.value }
+        //inserisco l'oggetto nel array team
         team.push(new_member)
+        //creo un elemento della dom che sarà la card
         const div = document.createElement('div')
+        //creo tre elementi della DOM che saranno figli diretti della card
         const div_1 = document.createElement('div')
         const div_2 = document.createElement('div')
         const img_element = document.createElement('img')
+        //ai tre figli diretti di cart appendo i rispettivi valori che vengono inseriti nel form
         div_1.append(name_1.value)
         div_2.append(role.value)
         img_element.setAttribute('src', `./img/${url.value}`);
+        //appendo i tre figli diretti della card alla card
         div.append(div_1)
         div.append(div_2)
         div.append(img_element)
+        //appendo la crd al container
         container.append(div)
         //pulisco il form
         name_1.value = ''
         role.value = ''
         url.value = ''
+        //loggo a console l'array team per verificare che il nuovo oggetto sia stato aggiunto 
+        console.log(team)
     }
 
 })
@@ -66,12 +80,12 @@ button_add.addEventListener('click', () =>{
 
 
 
-
+    //questo ciclo viene eseguito per ogni item dell'array
   for(let member of team){
       const div = document.createElement('div')
       container.append(div)
 
-      console.log(div)
+      //questo ciclo viene eseguito per ogni chiave/proprietà del oggetto
       for (let key in member){
         const div_2 = document.createElement('div')
         const img = document.createElement('img')
@@ -88,3 +102,4 @@ button_add.addEventListener('click', () =>{
       }
   }
 
+  
